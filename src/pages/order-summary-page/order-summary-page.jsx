67405b86853ae52830d964b1217600a35b02ade8
Hamza Es-saady <es.saady.hamza@gmail.com>
2023-10-25 15:28:26 +0100

    feat(order-summary): add scoops and toppings lists and prices from context

--- src/pages/order-summary-page/order-summary-page.jsx
+++ src/pages/order-summary-page/order-summary-page.jsx
@@ -8,8 +8,35 @@ import {
   CardTitle,
 } from "components/ui/card";
 import SummaryForm from "./components/summary-form/summary-form";
+import { useOrderDetails } from "contexts/order-details/use-order-details";
+import { formatCurrency } from "lib/utils";
 
 export default function OrderSummaryPage() {
+  const { getPrice, order } = useOrderDetails();
+  const { scoops: scoopsPrice, toppings: toppingsPrice, total } = getPrice();
+
+  const renderScoopsItems = () => {
+    const { scoops } = order;
+
+    return scoops
+      ? Object.entries(scoops).map(
+          ([name, count], index) =>
+            !!count && <li key={index}>{`x${count} ${name}`}</li>
+        )
+      : null;
+  };
+
+  const renderToppingsItems = () => {
+    const { toppings } = order;
+
+    return toppings
+      ? Object.entries(toppings).map(
+          ([name, count], index) =>
+            !!count && <li key={index}>{`x${count} ${name}`}</li>
+        )
+      : null;
+  };
+
   return (
     <div className={"h-screen flex flex justify-center items-center"}>
       <Card className="w-3/5 h-min my-10">
@@ -20,7 +47,33 @@ export default function OrderSummaryPage() {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <p>Scoops</p>
+          <div>
+            <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+              Scoops{" "}
+              <span className="text-cyan-500">
+                {formatCurrency(scoopsPrice)}
+              </span>
+            </h4>
+            <ul className="my-4 ml-11 list-disc [&>li]:mt-2">
+              {renderScoopsItems()}
+            </ul>
+          </div>
+
+          <div className="my-10">
+            <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+              Toppings{" "}
+              <span className="text-cyan-500">
+                {formatCurrency(toppingsPrice)}
+              </span>
+            </h4>
+            <ul className="my-4 ml-11 list-disc [&>li]:mt-2">
+              {renderToppingsItems()}
+            </ul>
+          </div>
+
+          <h4 className="scroll-m-20 text-2xl font-bold tracking-tight">
+            Total <span className="text-cyan-500">{formatCurrency(total)}</span>
+          </h4>
         </CardContent>
         <CardFooter>
           <SummaryForm />
