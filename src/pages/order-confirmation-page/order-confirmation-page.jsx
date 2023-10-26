c11405bd3b367aaac71b93b2b086a91366a0815b
Hamza Es-saady <es.saady.hamza@gmail.com>
2023-10-26 16:57:49 +0100

    feat(order-confirmation): fetch order number from server

--- src/pages/order-confirmation-page/order-confirmation-page.jsx
+++ src/pages/order-confirmation-page/order-confirmation-page.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { RotateCcw } from "lucide-react";
 import { Button } from "components/ui/button";
-
-const uid = () => String(Date.now().toString(32));
+import { API_URL } from "lib/constants";
+import { Loader2 as Loader } from "lucide-react";
 
 export default function OrderConfirmation() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [orderNumber, setOrderNumber] = useState(null);
+
+  useEffect(() => {
+    fetch(`${API_URL}/order`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setOrderNumber(data.orderNumber))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col justify-center items-center h-screen">
+        <Loader className="mr-2 h-16 w-16 animate-spin" />
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-10">
@@ -13,7 +36,7 @@ export default function OrderConfirmation() {
 
       <h2 className="text-xl font-semibold">
         Your order number is{" "}
-        <span className="text-muted-foreground">{uid()}</span>
+        <span className="text-muted-foreground">{orderNumber}</span>
       </h2>
 
       <p>As per our terms and conditions, nothing will happen now</p>
